d181e537a4ea8b45c310e26ccf44a760154086a1
simonelopess <sisilopes20@gmail.com>
2020-10-23 11:12:49 -0300

    Adicinado as maracações e evento de click

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { Icon } from 'leaflet';
+import * as parkData from './data/skateboard-parks.json';
 
 import './App.css';
 
 function App() {
+  const [activePark, setActivePark] = React.useState(null);
+
   return (
     <div className="App">
-      <Map center={[-22.895751, -43.240692]} zoom={32}>
+      <Map center={[45.421532, -75.697189]} zoom={12}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
         />
+        {parkData.features.map((park) => (
+          <Marker
+            key={park.properties.PARK_ID}
+            position={[
+              park.geometry.coordinates[1],
+              park.geometry.coordinates[0],
+            ]}
+            onclick={() => {
+              setActivePark(park);
+            }}
+          />
+        ))}
+        {activePark && (
+          <Popup
+            position={[
+              activePark.geometry.coordinates[1],
+              activePark.geometry.coordinates[0],
+            ]}
+            onClose={() => {
+              setActivePark(null);
+            }}
+          >
+            <div>
+              <h2>{activePark.properties.NAME}</h2>
+              <p>{activePark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        )}
       </Map>
     </div>
   );
